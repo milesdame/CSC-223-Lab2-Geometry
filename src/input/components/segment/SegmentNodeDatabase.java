0977b684a50b09f67bd6ccfc982be8414208fce1
Sophie-Ngo <sophiegngo@gmail.com>
2022-09-07 15:19:56 -0400

    wrote numDirectedEdges & comment segnode database
    title

--- src/input/components/segment/SegmentNodeDatabase.java
+++ src/input/components/segment/SegmentNodeDatabase.java
@@ -1,3 +1,12 @@
+/**
+ * A SegmentNodeDatabase is built upon an adjacency list that represents a geometry figure.
+ * The adjacency list is a LinkedHashMap.
+ * Each entry in this map is:
+ * - key = a PointNode that represents a vertex of the figure
+ * - value = a set of PointNodes that represents a directed edge from the key node to each node in the set
+ * 
+ * An undirected edge exists between two points in the adjacency list if both points exist as a key and they exist in each other's value sets.
+ */
 package input.components.segment;
 
 import java.util.AbstractSet;
@@ -19,6 +28,7 @@ public class SegmentNodeDatabase {
 	 */
 	public SegmentNodeDatabase() {
 		_adjLists = new LinkedHashMap<PointNode, Set<PointNode>>();
+
 	}
 	
 	/**
@@ -28,17 +38,27 @@ public class SegmentNodeDatabase {
 	 * 		  that is a point and a value that is a set of connected points
 	 */
 	public SegmentNodeDatabase(LinkedHashMap<PointNode, Set<PointNode>> map) {
-		_adjLists = new LinkedHashMap<PointNode, Set<PointNode>>();
-	}
-	
-	public int numUndirectedEdges() {
-		// TODO
+		_adjLists = new LinkedHashMap<PointNode, Set<PointNode>>(map);
 	}
 	
 	/**
-	 * 
-	 * @param n1
-	 * @param n2
+	 * Returns the amount of undirected edges in the adjacency list.
+	 * @return int value of the total amount of undirected edges in the adj list
+	 */
+	public int numUndirectedEdges() {
+		int sum = 0;
+		
+		for (Set<PointNode> set : _adjLists.values()) {
+			sum = sum + set.size();
+		}
+		
+		return sum;
+	}
+	
+	/**
+	 * Given two points, adds a directed edge from the first point to the second point.
+	 * @param n1 - the first PointNode
+	 * @param n2 - the second PointNode
 	 */
 	private void addDirectedEdge(PointNode n1, PointNode n2) {
 		Set<PointNode> pointSet = new LinkedHashSet<PointNode>();
@@ -47,9 +67,10 @@ public class SegmentNodeDatabase {
 	}
 	
 	/**
-	 * 
-	 * @param n1
-	 * @param n2
+	 * Given two points, adds an undirected edge between them.
+	 * This is attained by adding a directed edge between them in both directions.
+	 * @param n1 - the first PointNode (though order does not matter)
+	 * @param n2 - the second PointNode (though order does not matter)
 	 */
 	public void addUndirectedEdge(PointNode n1, PointNode n2) {
 		if (_adjLists.containsKey(n1) == true) { _adjLists.get(n1).add(n2);}
